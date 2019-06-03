216a12c976e566db56c2e9a2a647220f91148b2f
Serge <harjaus@coventry.ac.uk>
2019-06-03 15:17:32 +0100

    Initial work on 2 gate detection. 20fps

--- maskProcessing.py
+++ maskProcessing.py
@@ -29,36 +29,55 @@ def findMin(bigList, k):
 		min_val = 0	
 	return min_val
 
-def findFirstPlane(red, yellow, depth):
+def findGates(red, yellow, depth):
 	"""Inputs are red, yellow and depth mask."""
 
 	conesDepth = cv2.bitwise_and(depth, depth, mask=red+yellow)
 		#depth info just where the cones are
-
+#First Gate
 	planeDistance = findMin(conesDepth, 0.7)
-
 	markedPixels = np.zeros((len(depth), len(depth[0])), dtype=np.int8)
 
 	if planeDistance == 0: #no object in sight
-		return markedPixels, markedPixels #empty arrays
+		return markedPixels, markedPixels, markedPixels, markedPixels
+			#empty pixels
+	else:
+		maxFirstGate = planeDistance + 0.5
+		markedPixels = threshold(conesDepth, planeDistance, maxFirstGate, 0)
+			#only keep pixels in the desired threshold
 
-	markedPixels = threshold(conesDepth, planeDistance, planeDistance+1, 0)
-		#only keep pixels in the desired threshold
+		markedPixels[markedPixels>1] = 1 #if ever an element is bigger than 1, make it 1
+		markedPixels = (markedPixels.round()* 255).astype(np.uint8) #transform to desired format
 
-	markedPixels[markedPixels>1] = 1 #if ever an element is bigger than 1, make it 1
+		firstRed = cv2.bitwise_and(red, red, mask=markedPixels)
+		firstYellow = cv2.bitwise_and(yellow, yellow, mask=markedPixels)
 
-	markedPixels = (markedPixels.round()* 255).astype(np.uint8) #transform to desired format
+#Second Gate
+	secondGate = findMin(conesDepth, maxFirstGate + 1)
+	#print("Endpoint for firstGate: ", maxFirstGate)
+	#print("SecondGate startPoint: ", secondGate)
+	markedPixels = np.zeros((len(depth), len(depth[0])), dtype=np.int8)
+	#input()
+	if secondGate == 0:
+		return firstRed, firstYellow, markedPixels, markedPixels
+		#return what we have so far
+	else:
+		maxSecondGate = secondGate + 0.5
+		markedPixels = threshold(conesDepth, secondGate, maxSecondGate, 0)
+				#only keep pixels in the desired threshold
 
-	firstRed = cv2.bitwise_and(red, red, mask=markedPixels)
-	firstYellow = cv2.bitwise_and(yellow, yellow, mask=markedPixels)
+		markedPixels[markedPixels>1] = 1 #if ever an element is bigger than 1, make it 1
+		markedPixels = (markedPixels.round()* 255).astype(np.uint8) #transform to desired format
+		
+		secondRed = cv2.bitwise_and(red, red, mask=markedPixels)
+		secondYellow = cv2.bitwise_and(yellow, yellow, mask=markedPixels)
 
-	return firstRed, firstYellow
+	return firstRed, firstYellow, secondRed, secondYellow
 
 
 def findLineMarkers(red, yellow):
 
 	try:
-
 		redIndex = np.where(red==255)
 		redMarker = (redIndex[1][0], redIndex[0][0])
 
