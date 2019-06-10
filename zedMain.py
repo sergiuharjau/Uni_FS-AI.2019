bd717d705152470459e21323b6bd69d903b92a7b
Serge <harjaus@coventry.ac.uk>
2019-06-10 12:49:53 +0100

    Implemented threading. Not fully working.

--- zedMain.py
+++ zedMain.py
@@ -5,6 +5,7 @@ import cv2
 import time
 from colourDetection import findColour
 from maskProcessing import findGates, findLineMarkers
+import threading
 
 sys.path.insert(0, '../fspycan/lib/')
 import fspycan_ext
@@ -14,6 +15,7 @@ def issueCommands(steering, velocity):
 		issueCommands.car = fspycan_ext.Car("can0")
 	issueCommands.car.set_steering_velocity(int(steering), int(velocity))
 	issueCommands.car.loop()
+
 	
 def targetProcessing(target):
 	"""Deals with calculations based on target. Returns an average once every 15 frames."""
@@ -35,6 +37,73 @@ def targetProcessing(target):
 		targetProcessing.frameCounter = 0
 
 		return average
+
+def imCapt(zed, lock):
+	"""Used for parallelised image and depth campturing."""
+
+	print("Different thread started")
+	start = time.time()
+
+	zed.retrieve_image(imCapt.image_zed, sl.VIEW.VIEW_LEFT, sl.MEM.MEM_CPU)
+	zed.retrieve_measure(imCapt.depth_data_zed, sl.MEASURE.MEASURE_DEPTH, sl.MEM.MEM_CPU)
+
+	print("Different thread took: ", time.time()-start)
+
+def imProcessing(image_ocv, depth_data_ocv, visual, zed, original_image):
+	print("Started processing")
+	processing = time.time()
+
+	maskRed, maskYellow = findColour(image_ocv)
+	combinedMask = maskRed + maskYellow
+	print("Processed colour")
+	fRed, fYellow, secRed, secYellow = findGates(maskRed, maskYellow, depth_data_ocv)
+			 #finds the masks for the first red/yellow cones
+	print("Processed gate depth")
+	#FirstGate
+	redLine1, yellowLine1 = findLineMarkers(fRed, fYellow) 
+	target1 = (int((yellowLine1[0] + redLine1[0])/2), int((yellowLine1[1] + redLine1[1])/2))
+		#the center of the first gate
+	#SecondGate
+	redLine2, yellowLine2 = findLineMarkers(secRed, secYellow)			
+	target2 = (int((yellowLine2[0] + redLine2[0])/2), int((yellowLine2[1] + redLine2[1])/2))
+	print("Processed line markers")
+	reading = targetProcessing(target1) #averages a reading every 15 frames
+
+	print("All of processing took: ", time.time()-processing)
+		
+	if visual:
+		depth_image_zed = sl.Mat(1280, 720, sl.MAT_TYPE.MAT_TYPE_8U_C4)
+		zed.retrieve_image(depth_image_zed, sl.VIEW.VIEW_DEPTH, sl.MEM.MEM_CPU)
+		depth_image_ocv = depth_image_zed.get_data()
+
+		combinedImage = cv2.bitwise_and(image_ocv, image_ocv, mask=combinedMask)
+		redImage = cv2.bitwise_and(image_ocv, image_ocv, mask=maskRed)
+		yellowImage = cv2.bitwise_and(image_ocv, image_ocv, mask=maskYellow)
+		combinedImage = cv2.bitwise_and(image_ocv, image_ocv, mask=combinedMask)
+
+		cv2.imshow("firstGate", fRed + fYellow)
+		cv2.imshow("secondGate", secRed + secYellow)
+		cv2.imshow('colour data', combinedImage)
+		cv2.imshow('full depth', depth_image_ocv)
+
+
+	#FirstGate
+		if redLine1[0]:
+			cv2.line(original_image[270:300], redLine1, yellowLine1, (0,255,0), 10)
+			cv2.circle(original_image[270:300], target1, 5, (0,0,255), 4)
+			center = (int(1280/2), 0)
+			cv2.line(original_image[270:300], target1, center, (255,0,0), 2)
+	#SecondGate
+		if redLine2[0]:
+			cv2.line(original_image[270:300], redLine2, yellowLine2, (255,0,0), 5)
+			cv2.circle(original_image[270:300], target2, 5, (255,0,255), 2)
+
+		cv2.imshow("image", original_image)
+		cv2.imshow("cropped", image_ocv)
+
+		cv2.waitKey(10)
+
+	return reading
 
 def main(visual = False) :
 
@@ -66,76 +135,48 @@ def main(visual = False) :
 	height = 720
 
 	# Declare sl.Mat matrices
-	image_zed = sl.Mat(width, height, sl.MAT_TYPE.MAT_TYPE_8U_C4)
-	depth_image_zed = sl.Mat(width, height, sl.MAT_TYPE.MAT_TYPE_8U_C4)
+	imCapt.image_zed = sl.Mat(width, height, sl.MAT_TYPE.MAT_TYPE_8U_C4)
 	# Create a sl.Mat with float type (32-bit)
-	depth_data_zed = sl.Mat(width, height, sl.MAT_TYPE.MAT_TYPE_32F_C1)
+	imCapt.depth_data_zed = sl.Mat(width, height, sl.MAT_TYPE.MAT_TYPE_32F_C1)
 
 	skipped = 0
 	startTime = time.time()
-	framesToDo = 50000
+	framesToDo = 500
+	lock = threading.Lock()
 
 	for amount in range(framesToDo):
+		start = time.time()
 		err = zed.grab(runtime)
-		if err == sl.ERROR_CODE.SUCCESS :
+		if err == sl.ERROR_CODE.SUCCESS:
 			# Retrieve the left image, depth image in specified dimensions
-			zed.retrieve_image(image_zed, sl.VIEW.VIEW_LEFT, sl.MEM.MEM_CPU, int(width), int(height))
-			zed.retrieve_measure(depth_data_zed, sl.MEASURE.MEASURE_DEPTH, sl.MEM.MEM_CPU, int(width), int(height))
+			print("New frame")
 
-			original_image = image_zed.get_data()
+			transcribing = time.time()
+			original_image = imCapt.image_zed.get_data()
+			depth_data_ocv = imCapt.depth_data_zed.get_data()[270:300]
 			image_ocv = original_image[270:300]
-			depth_data_ocv = depth_data_zed.get_data()[270:300]
 
-			maskRed, maskYellow = findColour(image_ocv)
-			combinedMask = maskRed + maskYellow
+			print("Transcribing took: ", time.time()-transcribing)
 
-			fRed, fYellow, secRed, secYellow = findGates(maskRed, maskYellow, depth_data_ocv)
-					 #finds the masks for the first red/yellow cones
-			#FirstGate
-			redLine1, yellowLine1 = findLineMarkers(fRed, fYellow) 
-			target1 = (int((yellowLine1[0] + redLine1[0])/2), int((yellowLine1[1] + redLine1[1])/2))
-				#the center of the first gate
-			#SecondGate
-			redLine2, yellowLine2 = findLineMarkers(secRed, secYellow)			
-			target2 = (int((yellowLine2[0] + redLine2[0])/2), int((yellowLine2[1] + redLine2[1])/2))
+			print("Starting threading")
+			t = threading.Thread(target=imCapt, args=(zed,lock))
+			t.start()
+			print("Thread started, time elapsed from start: ", time.time()-start)
 
-			reading = targetProcessing(target1) #averages a reading every 15 frames
+			reading = imProcessing(image_ocv, depth_data_ocv, visual, zed, original_image)
+
 			if reading:
 				print("Camera: ", reading)
 				issueCommands((reading/20)*-1,50)
 
 			#print("Frames left: ", framesToDo-amount)
-
-			if visual:
-				zed.retrieve_image(depth_image_zed, sl.VIEW.VIEW_DEPTH, sl.MEM.MEM_CPU, width, height)
-				depth_image_ocv = depth_image_zed.get_data()
-
-				combinedImage = cv2.bitwise_and(image_ocv, image_ocv, mask=combinedMask)
-				redImage = cv2.bitwise_and(image_ocv, image_ocv, mask=maskRed)
-				yellowImage = cv2.bitwise_and(image_ocv, image_ocv, mask=maskYellow)
-				combinedImage = cv2.bitwise_and(image_ocv, image_ocv, mask=combinedMask)
-
-				cv2.imshow("firstGate", fRed + fYellow)
-				cv2.imshow("secondGate", secRed + secYellow)
-				cv2.imshow('colour data', combinedImage)
-				cv2.imshow('full depth', depth_image_ocv)
+			
+			join = time.time()
+			t.join()
+			print("Joining took:", time.time()-join)
+			print("Whole frame took: ", time.time() - start)
 
 
-			#FirstGate
-				if redLine1[0]:
-					cv2.line(original_image[270:300], redLine1, yellowLine1, (0,255,0), 10)
-					cv2.circle(original_image[270:300], target1, 5, (0,0,255), 4)
-					center = (int(width/2), 0)
-					cv2.line(original_image[270:300], target1, center, (255,0,0), 2)
-			#SecondGate
-				if redLine2[0]:
-					cv2.line(original_image[270:300], redLine2, yellowLine2, (255,0,0), 5)
-					cv2.circle(original_image[270:300], target2, 5, (255,0,255), 2)
-
-				cv2.imshow("image", original_image)
-				cv2.imshow("cropped", image_ocv)
-
-				cv2.waitKey(10)
 			
 		else:
 			skipped += 1
@@ -149,4 +190,4 @@ def main(visual = False) :
 	print("\nFINISH")
 
 if __name__ == "__main__":
-	main(True) 
+	main(False) 
