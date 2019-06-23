7ef42f0b857261c9a26f237fd174e3a1a37d3b4b
Serge <harjus@uni.coventry.ac.uk>
2019-06-23 14:53:04 +0100

    Functionally better, now we can reset the bench for a certain mission

--- src/plotter.py
+++ src/plotter.py
@@ -1,20 +1,54 @@
 import matplotlib.pyplot as plt
+import sys
 
-f1 = open("../test/mission5/commands.txt", "r")
-rawComs = f1.read().split(",")
+def plotCommands(path1, path2=None):
 
-commands = [0]
+    f1 = open(path1, "r")
+    rawComs = f1.read().split(",")
 
-for com in rawComs:
-    if com == "None" or com == "":
-        com = commands[-1]
-    commands.append(int(com)) #offset from last one
+    commands = [0]
 
-commands[:] = [x / 100 for x in commands]
+    for com in rawComs:
+        if com == "None" or com == "":
+            com = commands[-1]
+        commands.append(int(com)) #offset from last one
 
-frames = range(len(commands))
+    commands[:] = [x / 100 for x in commands]
+    frames = range(len(commands))
 
-plt.plot(commands, frames)
-plt.xlabel("Commands")
-plt.ylabel("Frames")
-plt.show()
+    plt.plot(commands, frames, "green") #green for benchmark
+    plt.xlabel("Commands")
+    plt.ylabel("Frames")
+
+    if path2:
+        f2= open(path2, "r")
+        rawComs2 = f2.read().split(",")
+
+        commands = [0]
+
+        for com in rawComs2:
+            if com == "None" or com == "":
+                com = commands[-1]
+            commands.append(int(com)) #offset from last one
+
+        commands[:] = [x / 100 for x in commands]
+        frames = range(len(commands))
+
+        plt.plot(commands, frames, "blue") #blue for newCom
+
+    plt.show()
+
+if __name__ == "__main__":
+
+    setBench = False #sets past mission commands to Benchmark for given mission
+    mission = 5
+
+    for argument in sys.argv[1:]:
+        exec(argument)
+
+    if setBench:
+        open("../test/mission" + str(mission) + "/commands.txt", "w").write(open("../test/pastMission.txt", "r").read())
+        print("Past mission commands.txt is now the new benchmark.")
+        quit()
+
+    plotCommands("../test/mission" + str(mission) + "/commands.txt", "../test/pastMission.txt")
