e159c70dd25e72e4ac2e9743ec83c61639871b93
Serge Matebook <harjaus@uni.coventry.ac.uk>
2019-07-10 14:10:21 +0100

    Now stops gps properly

--- src/acceleration.py
+++ src/acceleration.py
@@ -10,13 +10,18 @@ if __name__ == "__main__":
 
 	startingPos = gps.coords()
 	print("GPS lock acquired, starting position: ", startingPos)
-
+	
 	distanceAway = 0
 	while distanceAway < 100:
+		coordinates = gps.coords()
+		if coordinates == (0,0):
+			print("Lost GPS. Stopping until we regain")
+			issueCommands(0,0)
 		distanceAway = (distance.distance(gps.coords(), startingPos)).m
 		print("Distance away from destination: ", distanceAway)
 		issueCommands(0,150)
+		time.sleep(0.2)
 
 	print("Reached destination, stopping.")
 	issueCommands(0,0,True)
-
+    gps.stop()
