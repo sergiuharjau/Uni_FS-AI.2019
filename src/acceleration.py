6ef6e01d00451f867e361d238076848db0f195aa
Serge Matebook <harjaus@uni.coventry.ac.uk>
2019-07-10 07:41:23 +0100

    Created acceleration mission

---
+++ src/acceleration.py
@@ -0,0 +1,22 @@
+from cmds import issueCommands
+from gps import GPS 
+from geopy import distance
+
+if __name__ == "__main__":
+	gps = GPS(logging=False)
+
+	while gps.coords() == (0,0):
+		print("Awaiting gps lock.")
+
+	startingPos = gps.coords()
+	print("GPS lock acquired, starting position: ", startingPos)
+
+	distanceAway = 0
+	while distanceAway < 100:
+		distanceAway = (distance.distance(gps.coords(), startingPos)).m
+		print("Distance away from destination: ", distanceAway)
+		issueCommands(0,150)
+
+	print("Reached destination, stopping.")
+	issueCommands(0,0,True)
+
