0dd325e57b2484b665dd4fe056dc865dc29daba1
Serge Matebook <harjaus@uni.coventry.ac.uk>
2019-07-11 17:12:01 +0100

    Re-added GPS lines

--- src/acceleration.py
+++ src/acceleration.py
@@ -4,14 +4,15 @@ from geopy import distance
 import time
 
 if __name__ == "__main__":
-#	gps = GPS(logging=False)
+	gps = GPS()
 
-#	while gps.getCoords() == (0,0):
-#		print(gps.getCoords())
-#		print("Awaiting gps lock.")
+	while gps.getCoords() == (0,0):
+		print(gps.getCoords())
+		print("Awaiting gps lock.")
+		time.sleep(1)
 
-#	startingPos = gps.getCoords()
-#	print("GPS lock acquired, starting position: ", startingPos)
+	startingPos = gps.getCoords()
+	print("GPS lock acquired, starting position: ", startingPos)
 	issueCommands(0,0) # until it connects
 
 	startLeft = issueCommands.car.get_left_pulse()
@@ -19,18 +20,18 @@ if __name__ == "__main__":
 	start = time.time()
 	distanceAway = 0
 	while distanceAway < 50:
-#		coordinates = gps.coords()
-#		if coordinates == (0,0):
-#			print("Lost GPS. Stopping until we regain")
-#			issueCommands(0,0)
-#		distanceAway = (distance.distance(gps.getCoords(), startingPos)).m
+		coordinates = gps.coords()
+		if coordinates == (0,0):
+			print("Lost GPS. Stopping until we regain")
+			issueCommands(0,0)
+		distanceAway = (distance.distance(gps.getCoords(), startingPos)).m
 		print("Distance away from destination: ", distanceAway)
-		issueCommands(0,100)
-		time.sleep(2)
+		issueCommands(0,150)
+		time.sleep(4)
 	print("Total time: ", time.time()-start)
 	print("Total left pulse: ", issueCommands.car.get_left_pulse() - startLeft)
 	print("Total right pulse: ", issueCommands.car.get_right_pulse() - startRight)
 	print("Reached destination, stopping.")
 
 	issueCommands(0,0,True)
-#	gps.stop()
+	gps.stop()
