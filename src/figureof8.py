af84852943264342621c898f898749df8c1a65e3
Serge Matebook <harjaus@uni.coventry.ac.uk>
2019-07-15 22:47:37 +0100

    A bunch of code. It probably doesn't work. Send help

--- src/figureof8.py
+++ src/figureof8.py
@@ -1,8 +1,15 @@
-from cmds import issueCommands
-from gps import GPS
 import math
 import time
+import cv2
+import logging
+
 from geopy import distance
+import threading
+from imProc import imProcessing
+from capturing import ImageCap
+from cmds import calculateReading, issueCommands
+from globals import pixelStrip, startFrom, logInitial
+from gps import GPS
 
 if __name__ == "__main__":
 
@@ -30,7 +37,22 @@ if __name__ == "__main__":
 	startRight = issueCommands.car.get_right_pulse() #should be 0 
 	startPulse = int((startLeft + startRight)/2)
 
-	flip = 1 #goes right first
+	flip = 0 #goes right first
+
+			
+	calculateReading.pastCom = 0  # in case we don't see cones straight away
+	ic = ImageCap(False, replay)  # ImCapt() #initializes zed object
+	logging.basicConfig(filename='loggingMain.log', format='%(asctime)s %(message)s', level=logging.INFO)
+    logging.warning("\nMission start!\n")
+
+    visual= False; green= False; record= False; replay= False; loop= True; rc=False; cFlip=0 ;
+    eight = True; swapCircles=0#goes right first
+    followGreen=0 
+
+    for argument in sys.argv[1:]:
+        exec(argument)
+
+    startTime = time.time()
 
 	try: 
 		while True:
@@ -39,19 +61,38 @@ if __name__ == "__main__":
 			rightPulse=issueCommands.car.get_right_pulse()
 			averagePulse = int((leftPulse+rightPulse)/2)
 
-			
 
-			issueCommands(flip*-24, 85) #go slightly right 
-			
-			
+ 			image, depth = ic.latest(record)
+            logging.info("Getting latest image and depth.")
+            t = threading.Thread(target=ImageCap.capture, args=(ic, ))
+
+            original_image = image
+            depth_data_ocv = depth[startFrom:startFrom + pixelStrip]
+            image_ocv = original_image[startFrom:startFrom + pixelStrip]
+
+        	steering, velocity = imProcessing(t, image_ocv, depth_data_ocv, visual, original_image, green, cFlip, eight, swapCircles, followGreen)
+
+        	if (time.time() - followedFor) > 10:
+        		steering = 0
+   				velocity = 100
+
+            print("Steering: ", steering)
+            print("Velocity: ", velocity)
+            logging.info("Steering: %d, Velocity: %d", steering, velocity)
+
+			issueCommands(steering, velocity)
 
 			if (averagePulse - startPulse) > 50: #circumference of circle / wheel circumeference * Pulse/rotation (probably 20)
 				if (distance.distance(gps.getCoords(), centerGPS)).m < 2:
 					print("Reached center")
 					count += 1
 					if count == 2:
+						if swapCircles == 1: # 4th full circle we do
+							followGreen = 1
+							startingPos = gps.getCoords()
+							followedFor = time.time()
 						count = 0
-						flip = -1 #so we go the other way
+						swapCircles = 1 #so we go the other way
 						print("Flipped")
 					else:
 						print("First lap. Doing another.")
@@ -60,9 +101,27 @@ if __name__ == "__main__":
 					print("Pulses say we should be centered, but GPS doesn't agree.")
 					print("GPS: ", gps.getCoords())
 
+		    if followGreen:
+				distanceAway = (distance.distance(gps.getCoords(), startingPos)).m
+				print("Distance away from destination: ", distanceAway)
+				if distanceAway > 20: #after 20m of following green
+					raise KeyboardInterrupt
+
 				
 	except KeyboardInterrupt:
+		issueCommands(0,0)
+		time.sleep(4)
 		issueCommands(0,0, True)
+
+        print("Seconds it took: ", time.time() - startTime)
+        print("Total frames: ", len(listReadings))
+        print("Actual framerate: ", len(listReadings) / (time.time() - startTime))
+        print("\nFINISH")
+
+        logging.info("\nTotal seconds: %d", time.time()-startTime)
+        logging.info("Framerate: %d", len(listReadings) / (time.time() - startTime))
+        logging.warning("Mission end.\n\n")
+        quit()
 #		gps.stop()
 
 		##calculate if we've done a full circle yet 
