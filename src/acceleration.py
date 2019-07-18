d9e03bfa7631b625e3017902f535214bf9af96ab
Serge Matebook <harjaus@uni.coventry.ac.uk>
2019-07-18 15:51:55 +0100

    First iteration of new acceleration code

--- src/acceleration.py
+++ src/acceleration.py
@@ -1,38 +1,113 @@
-from cmds import issueCommands
-from gps import GPS 
-from geopy import distance
+import sys
 import time
+import threading
+from imProc import imProcessing
+from capturing import ImageCap
+from cmds import calculateReading, issueCommands
+from globals import pixelStrip, startFrom, logInitial
+import cv2
+import logging
+from gps import GPS
+
+def main(visual, green, record, replay, loop, rc, cFlip):
+
+    calculateReading.pastCom = 0  # in case we don't see cones straight away
+
+    ic = ImageCap(False, replay)  # ImCapt() #initializes zed object
+    startTime = time.time()
+    listReadings = []
+
+    gps = GPS()
+
+    while gps.getGPS() == (0,0):
+        print("Waiting on GPS lock.")
+        time.sleep(1)
+
+    startingPos = gps.getGPS()
+
+    try:
+        i=0
+        while loop:  # for amount in range(framesToDo):
+            logging.warning("\n*********\nFrame: " + str(i))
+            image, depth = ic.latest(record)
+            logging.info("Getting latest image and depth.")
+            t = threading.Thread(target=ImageCap.capture, args=(ic, ))
+
+            if not record:
+                original_image = image
+                depth_data_ocv = depth[startFrom:startFrom + pixelStrip]
+                image_ocv = original_image[startFrom:startFrom + pixelStrip]
+
+                steering, velocity = imProcessing(t, image_ocv, depth_data_ocv, visual, original_image, green, cFlip, gates=0)
+            else:                                                                                           #looks for one gate
+                steering,velocity = 0,0
+                cv2.imshow("image", image)
+                cv2.waitKey(1)
+             # works faster here for performance reasons
+
+            if ic.exit:  # when we replay tests
+                raise KeyboardInterrupt
+
+            steering = min(19, max(-19, steering))
+            
+            velocity = 200
+
+            if distance.distance(gps.getGPS(), startingPos).m > 15: #change to 75 later
+                raise KeyboardInterrupt
+
+            print("Steering: ", steering)
+            print("Velocity: ", velocity)
+
+            logging.info("Steering: %d, Velocity: %d", steering, velocity)
+            
+            issueCommands(steering, velocity, False, visual, replay, record, rc)
+
+            listReadings.append(steering)
+
+            if not isinstance(loop, bool):
+                loop -= 1
+                if loop == 0:
+                    raise KeyboardInterrupt
+            t.join()
+            i+=1
+            logging.warning("End of frame.\n\n")
+
+            #print("Frames left: ", framesToDo-amount)
+
+    except KeyboardInterrupt:
+        if not replay:
+            ic.zed.close()
+        issueCommands(0,0)
+        print("We've set velocity to 0. Waiting to stop now.")
+        time.sleep(10) #increase in future if needed
+        issueCommands(0, 0, True, visual, replay, record, rc) #initiates the exit protocol
+
+        f1 = open("../test/pastMission.txt", "w")
+        for element in listReadings:
+            f1.write(str(element) + ",")
+        f1.close()
+
+        print("Seconds it took: ", time.time() - startTime)
+        print("Total frames: ", len(listReadings))
+        print("Actual framerate: ", len(listReadings) / (time.time() - startTime))
+        print("\nFINISH")
+
+        logging.info("\nTotal seconds: %d", time.time()-startTime)
+        logging.info("Framerate: %d", len(listReadings) / (time.time() - startTime))
+        logging.warning("Mission end.\n\n")
+        quit()
+
 
 if __name__ == "__main__":
-	gps = GPS()
 
-	while gps.getCoords() == (0,0):
-		print(gps.getCoords())
-		print("Awaiting gps lock.")
-		time.sleep(1)
+    visual= False; green= False; record= False; replay= False; loop= True; rc=False; cFlip=0 ;
 
-	startingPos = gps.getCoords()
-	print("GPS lock acquired, starting position: ", startingPos)
-	issueCommands(0,0) # until it connects
+    for argument in sys.argv[1:]:
+        exec(argument)
 
-	startLeft = issueCommands.car.get_left_pulse()
-	startRight = issueCommands.car.get_right_pulse()
-	start = time.time()
-	distanceAway = 0
-	while distanceAway < 75: #stops after 75m
-		coordinates = gps.coords()
-		if coordinates == (0,0):
-			print("Lost GPS. Stopping until we regain")
-			issueCommands(0,0)
-		distanceAway = (distance.distance(gps.getCoords(), startingPos)).m
-		print("Distance away from destination: ", distanceAway)
-		issueCommands(0,150)
-		time.sleep(1)	
-	print("Total time: ", time.time()-start)
-	print("Total left pulse: ", issueCommands.car.get_left_pulse() - startLeft)
-	print("Total right pulse: ", issueCommands.car.get_right_pulse() - startRight)
-	print("Reached destination, stopping.")
-	issueCommands(0,0)
-	time.sleep(10)
-	issueCommands(0,0,True)
-	gps.stop()
+    logging.basicConfig(filename='loggingMain.log', format='%(asctime)s %(message)s', level=logging.INFO)
+    logging.warning("\nMission start!\n")
+
+    logInitial()
+
+    main(visual, green, record, replay, loop, rc, cFlip)
