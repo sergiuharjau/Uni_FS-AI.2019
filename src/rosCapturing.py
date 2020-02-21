b54c35ac5cf1c1fe2c06b2bdba4a2f1ee1789089
harjaus <harjaus@coventry.ac.uk>
2020-02-21 11:13:28 +0000

    Added right type depth image

--- src/rosCapturing.py
+++ src/rosCapturing.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from __future__ import print_function
 
 import sys
 import rospy
@@ -10,20 +9,28 @@ from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import time
 
-class image_converter:
+class Image_converter:
 
   def __init__(self):
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("image_topic", Image, self.callback)
     print("Subscribed to topic")
-    self.cv_image = None
+    height = 720 ; width = 1280
+    self.cv_image = np.zeros((height,width,3), np.uint8)
+    print(self.cv_image)
+    self.cv_depth = np.zeros((height,width,1), np.uint8)
 
   def callback(self,data):
-    print("Got image")
+    print("Got image"*25)
     try:
       self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      print(type(self.cv_image))
+      input()
     except CvBridgeError as e:
       print(e)
+
+  def latest(self):
+    return self.cv_image, self.cv_depth
 
     #cv2.imshow("image", self.cv_image)
     #cv2.waitKey(3)
@@ -35,5 +42,5 @@ def mainRosNode(args=[]):
   rospy.spin()
 
 if __name__ == '__main__':
-    ic = image_converter()
+    ic = Image_converter()
     mainRosNode(sys.argv)
