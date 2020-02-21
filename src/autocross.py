b8c6ca81e9d6474792bb960e0743dc0865963ef9
harjaus <harjaus@coventry.ac.uk>
2020-02-21 11:13:46 +0000

    Beginning of integration with ROS. Currently doesnt work

--- src/autocross.py
+++ src/autocross.py
@@ -2,27 +2,30 @@ import sys
 import time
 import threading
 from imProc import imProcessing
-from capturing import ImageCap
+#from capturing import ImageCap
+from rosCapturing import Image_converter, mainRosNode 
 from cmds import calculateReading, issueCommands
 from globals import pixelStrip, startFrom, logInitial
 import cv2
 import logging
-from gps import GPS
-from geopy import distance
 
-def main(visual, green, record, replay, loop, rc, cFlip):
+#from gps import GPS
+#from geopy import distance
 
-"""//Used when stopping is needed.
-    gps = GPS()
-    while gps.getGPS(force=1) == (0,0):
-        print("Awaiting GPS lock.")
-        time.sleep(1)
-"""
+def main(visual, green, record, replay, loop, rc, cFlip, ic):
+# //Used when stopping is needed.
+#     gps = GPS()
+#     while gps.getGPS(force=1) == (0,0):
+#         print("Awaiting GPS lock.")
+#         time.sleep(1) 
+# 
 
-    ic = ImageCap(False, replay)  # ImCapt() #initializes zed object
-  
-    if not visual and not rc: #initalizes CAN connection
-        issueCommands(0,0)
+    #ic = ImageCap(False, replay)  # ImCapt() #initializes zed object
+
+    
+
+    #if not visual and not rc: #initalizes CAN connection
+        #issueCommands(0,0)
 
     calculateReading.pastCom = 0  # in case we don't see cones straight away
     startTime = time.time()
@@ -36,9 +39,9 @@ def main(visual, green, record, replay, loop, rc, cFlip):
         while loop:  # for amount in range(framesToDo):
             time1 = time.time()
             logging.warning("\n*********\nFrame: " + str(i))
-            image, depth = ic.latest(record)
+            image, depth = ic.latest()
             logging.info("Getting latest image and depth.")
-            t = threading.Thread(target=ImageCap.capture, args=(ic, ))
+            
 
             if not record:
                 original_image = image
@@ -51,37 +54,36 @@ def main(visual, green, record, replay, loop, rc, cFlip):
                 cv2.imshow("image", image)
                 cv2.waitKey(1)
 
-            if ic.exit:  # when we replay tests
-                raise KeyboardInterrupt
+            #if ic.exit:  # when we replay tests
+            #    raise KeyboardInterrupt
 
 #            steering = min(19, max(-19, steering))
 #Uncomment to cap steering to a certain boundary
 
 
-""" //To be used when counting laps
-
-            if setStart:
-                velocity -= 30
-                if time.time()-startTime > 12:
-                    startingPos = gps.getGPS()
-                    setStart = False
-                    gps.coords = (-1,-1)
-            if time.time() - timeMarker > 70:
-               velocity -= 50
-            if time.time()-timeMarker > 75: #only checks 30s after we've passed the starting point
-                gps.getGPS(timeBound=2)
-                if distance.distance(gps.getCoords(), startingPos).m < 7: #5m within the finish line
-                    lapCounter += 1
-                    timeMarker = time.time() #resets the time marker
-                    if lapCounter == 10: #change to 10 in the future 
-                        raise KeyboardInterrupt
-"""          
+# """ //To be used when counting laps
+#             if setStart:
+#                 velocity -= 30
+#                 if time.time()-startTime > 12:
+#                     startingPos = gps.getGPS()
+#                     setStart = False
+#                     gps.coords = (-1,-1)
+#             if time.time() - timeMarker > 70:
+#                velocity -= 50
+#             if time.time()-timeMarker > 75: #only checks 30s after we've passed the starting point
+#                 gps.getGPS(timeBound=2)
+#                 if distance.distance(gps.getCoords(), startingPos).m < 7: #5m within the finish line
+#                     lapCounter += 1
+#                     timeMarker = time.time() #resets the time marker
+#                     if lapCounter == 10: #change to 10 in the future 
+#                         raise KeyboardInterrupt
+# """          
             print("Steering: ", steering)
             print("Velocity: ", velocity)
 
             logging.info("Steering: %d, Velocity: %d", steering, velocity)
             
-            issueCommands(steering, velocity, False, visual, replay, record, rc)
+            #issueCommands(steering, velocity, False, visual, replay, record, rc)
 
             listReadings.append(steering)
 
@@ -89,11 +91,11 @@ def main(visual, green, record, replay, loop, rc, cFlip):
                 loop -= 1
                 if loop == 0:
                     raise KeyboardInterrupt
-            t.join()
-
-            if 'car' in issueCommands.__dict__:
-                  if issueCommands.car.checkEBS():
-                      raise KeyboardInterrupt
+            
+            time.sleep(1)
+            # if 'car' in issueCommands.__dict__:
+            #       if issueCommands.car.checkEBS():
+            #           raise KeyboardInterrupt
             i+=1
 
             logging.warning("End of frame.\n\n")
@@ -103,16 +105,17 @@ def main(visual, green, record, replay, loop, rc, cFlip):
             #print("Frames left: ", framesToDo-amount)
 
     except KeyboardInterrupt:
-        if not replay:
-            ic.zed.close()
-        issueCommands(0, 0, False, visual, replay, record, rc)
-        time.sleep(4)
-        issueCommands(0, 0, True, visual, replay, record, rc) #initiates the exit protocol
+        # if not replay:
+        #     ic.zed.close()
+        # issueCommands(0, 0, False, visual, replay, record, rc)
+        # time.sleep(4)
+        # issueCommands(0, 0, True, visual, replay, record, rc) #initiates the exit protocol
 
         f1 = open("../test/pastMission.txt", "w")
         for element in listReadings:
             f1.write(str(element) + ",")
         f1.close()
+        t.join()
 
         print("Seconds it took: ", time.time() - startTime)
         print("Total frames: ", len(listReadings))
@@ -137,4 +140,20 @@ if __name__ == "__main__":
 
     logInitial()
 
-    main(visual, green, record, replay, loop, rc, cFlip)
+    ic = Image_converter()
+    t = threading.Thread(target=main, args=(visual, green, record, replay, loop, rc, cFlip, ic))
+    t.start()
+    
+    mainRosNode()
+    t.join()
+    #main(visual, green, record, replay, loop, rc, cFlip)
+
+    """
+    What we need to do:
+
+    call the old FS-AI code in the CALLBACK of rosCapturing.py
+
+    So really flip everything on its head. 
+
+    Good luck Future Serge. 
+    """
